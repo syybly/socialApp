c88af62e432869d3d27e229884f77ae22126b86b
sylvia <shaoyueyingswc@gmail.com>
2022-09-24 16:36:11 +1000

    Clinet log in and register

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_modules/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model:any = {}
+  loggedIn: boolean
+  constructor(public accountService:AccountService) { }
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+   
+  }
+
+  login () {
+    this.accountService.login(this.model).subscribe(response=> {
+      console.log(response);
+      
+    }, error=> {
+      console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    
+
+  }
+
+  getCurrentUser () {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error=> {
+      console.log(error);
+    })
+  }
+
+}
